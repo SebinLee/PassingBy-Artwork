fac6ed1687f667e748206eaaada946854a04932e
SebinLee <lsbjeju@gmail.com>
2019-11-06 15:11:11 +0900

    Edit Code to Draw Outerline
    Edit Code to Draw Outerline, not a ConvexHull

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import cv2
+import time
 
 """
 OpenCV Python을 이용해 Camera에서 실시간 영상을 받아옵니다.
@@ -12,6 +13,9 @@ capture.set(cv2.CAP_PROP_FRAME_HEIGHT,720)
 
 trieditonce = False
 
+framerate = 30
+framerate_waittime= 1.0/framerate
+
 while True :
 
     """
@@ -20,41 +24,38 @@ while True :
     - frame : Camera의 데이터를 가져다줍니다.
     """
 
-    try :
-        ret, frame = capture.read()
+    ret, frame = capture.read()
 
-        if ret :
+    if ret :
 
-            thresholdBoundary = 150
-            maxValue = 255
+        thresholdBoundary = 150
+        maxValue = 255
+
+        """
+        cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) : frame을 Grayscale로 변환(색상변환)을 해주는 코드
+        cv2.threshold(grayscaled_frame, boundary, maxValue, cv2.THRESH_BINARY) : Grayscale로 변환된 Frame을 Boundary 값을 기준으로 이진화 해주는 코드
+        cv2.bitwise_not(frame) : frame을 반전시켜주는 코드
+        cv2.findContours(binaryFrame, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE) : binaryframe에서 Contour 좌표값을 가져오는 코드
+        """
+
+        grayscale = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+        _, binary = cv2.threshold(grayscale,thresholdBoundary,maxValue,cv2.THRESH_BINARY)
+        binary = cv2.bitwise_not(binary)
+        contours, hierachy = cv2.findContours(binary,cv2.RETR_CCOMP,cv2.CHAIN_APPROX_NONE)
 
 
-            grayscale = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-            _, binary = cv2.threshold(grayscale,thresholdBoundary,maxValue,cv2.THRESH_BINARY)
-            binary = cv2.blur(binary,(10,10))
-            #cv2.imshow("VideoThreshold",binary)
+        for i in range(len(contours)) : cv2.drawContours(grayscale, [contours[i]], 0, (0, 0, 255), 2)
+        cv2.imshow("src", grayscale)
 
-            contours, hierachy = cv2.findContours(binary,cv2.RETR_CCOMP,cv2.CHAIN_APPROX_NONE)
-
-            for i in contours :
-                #hull은 numpy.ndarray type이기 때문에 list()가 아니라 []로 변환한다.
-                hull = [cv2.convexHull(i,clockwise = True)]
-
-                #점들의 위치 정보는 hull[0]에 들어있기 때문에 hull[0]을 조사합니다.
-                if len(hull[0]) > 5 :
-                    cv2.drawContours(grayscale,hull,0,(0,0,255),2)
-
-            cv2.imshow("Windows with Threshold",grayscale)
-        
-        else :
-            print("NO CAMERA DETECT")
-            break
-
-        if cv2.waitKey(1) > 0 : break
-
-    except :
-        print("No Camera Detect - With Exception")
+    
+    else :
+        print("NO CAMERA DETECT")
         break
+
+    if cv2.waitKey(1) > 0 : break
+
+    time.sleep(framerate_waittime)
+
 
 print("CAPTURE END")
 capture.release()
