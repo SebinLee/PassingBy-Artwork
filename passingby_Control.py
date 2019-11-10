c28bfa65b4911859674197d6116c6c9a80cac8f2
SebinLee <lsbjeju@gmail.com>
2019-11-11 00:32:44 +0900

    Edit Control Panel
    Edit Control Panel UI and Code

--- passingby_Control.py
+++ passingby_Control.py
@@ -2,6 +2,8 @@ import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5 import uic
+import passingby_Artwork
+import threading
 
 form_class = uic.loadUiType("controlPanel.ui")[0]
 
@@ -10,11 +12,17 @@ class WindowClass(QMainWindow, form_class) :
         super().__init__()
         self.setupUi(self)
 
+        self.threshold = self.sliderThreshold.value()
+        self.cameraOpacity = float(self.sliderCamera.value())/100
+        self.framerate = self.sliderFrameRate.value()
+        self.listsize = self.sliderListSize.value()
+
+
         #Initialize Current Value Text
-        self.txtThreshold.setText(str(self.sliderThreshold.value()))
-        self.txtCamera.setText(str(1.0/self.sliderCamera.value()))
-        self.txtFrameRate.setText(str(self.sliderFrameRate.value()))
-        self.txtListSize.setText(str(self.sliderListSize.value()))
+        self.txtThreshold.setText(str(self.threshold))
+        self.txtCamera.setText(str(self.cameraOpacity))
+        self.txtFrameRate.setText(str(self.framerate))
+        self.txtListSize.setText(str(self.listsize))
 
         #Link Functions to Slider
         self.sliderThreshold.valueChanged.connect(self.editThreshold)
@@ -22,21 +30,34 @@ class WindowClass(QMainWindow, form_class) :
         self.sliderFrameRate.valueChanged.connect(self.editFrameRate)
         self.sliderListSize.valueChanged.connect(self.editListSize)
 
+        self.btnStart.clicked.connect(self.startArtwork)
+        self.btnTest.clicked.connect(self.testArtwork)
         
     def editThreshold(self) :
         self.txtThreshold.setText(str(self.sliderThreshold.value()))
+        self.threshold = self.sliderThreshold.value()
         
     def editCameraOpacity(self) :
         self.txtCamera.setText(str(1.0/self.sliderCamera.value()))
-
+        self.cameraOpacity = float(self.sliderCamera.value())/100
+        
     def editFrameRate(self) :
         self.txtFrameRate.setText(str(self.sliderFrameRate.value()))
+        self.framerate = self.sliderFrameRate.value()
 
     def editListSize(self) :
         self.txtListSize.setText(str(self.sliderListSize.value()))
+        self.listsize = self.sliderListSize.value()
+
+    def startArtwork(self) :
+        passingby_Artwork.artwork(self.threshold, self.cameraOpacity, self.framerate, self.listsize,False)
+
+    def testArtwork(self) :
+        passingby_Artwork.artwork(self.threshold, self.cameraOpacity, self.framerate, self.listsize,True)
+
 
 if __name__ == "__main__" :
     app = QApplication(sys.argv)
     myWindow = WindowClass()
     myWindow.show()
-    app.exec_()
+    app.exec_() 
